2e2a4888d07ac6426ea6fb73a2481a6141b68eec
Felix Ableitner <me@nutomic.com>
2022-05-04 01:04:35 +0200

    Use lemmy structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 use std::time::Duration;
 
 pub mod crawl;
-pub mod federated_instances;
-pub mod node_info;
 
 pub const REQUEST_TIMEOUT: Duration = Duration::from_secs(10);
 pub const DEFAULT_START_INSTANCES: &str = "lemmy.ml";
